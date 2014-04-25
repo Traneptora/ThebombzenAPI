80b05e11cf5d533743e800cd12d0cd63757ce03c
Leo Izen <leo.izen@gmail.com>
2014-04-25 16:48:11 -0400

    Removed info-less constructors

--- src/thebombzen/mods/thebombzenapi/configuration/ConfigFormatException.java
+++ src/thebombzen/mods/thebombzenapi/configuration/ConfigFormatException.java
@@ -3,20 +3,12 @@ package thebombzen.mods.thebombzenapi.configuration;
 public class ConfigFormatException extends Exception {
 	private static final long serialVersionUID = 1L;
 	
-	public ConfigFormatException(){
-		super();
-	}
-	
 	public ConfigFormatException(String info){
 		super(info);
 	}
 	
 	public ConfigFormatException(String info, Throwable cause){
 		super(info, cause);
-	}
-	
-	public ConfigFormatException(Throwable cause){
-		super(cause);
 	}
 	
 }
