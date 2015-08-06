3ca821e8aa3d03f7dda3a75a6814d6254488f900
Leo Izen <leo.izen@gmail.com>
2015-08-06 09:14:27 -0400

    Updated constants version

--- src/thebombzen/mods/thebombzenapi/Constants.java
+++ src/thebombzen/mods/thebombzenapi/Constants.java
@@ -1,6 +1,6 @@
 package thebombzen.mods.thebombzenapi;
 
 public interface Constants {
-	public static final String VERSION = "2.5.0";
-	public static final String MC_VERSION = "1.8";
+	public static final String VERSION = "2.4.2";
+	public static final String MC_VERSION = "1.7.10";
 }
