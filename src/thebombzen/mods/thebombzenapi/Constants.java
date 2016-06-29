9d09475958e92a9df257260e13fcbd8def05b52e
Leo Izen <leo.izen@gmail.com>
2016-06-29 13:18:59 -0400

    bumped version

--- src/thebombzen/mods/thebombzenapi/Constants.java
+++ src/thebombzen/mods/thebombzenapi/Constants.java
@@ -9,9 +9,9 @@ public interface Constants {
 	/**
 	 * The current version of ThebombzenAPI.
 	 */
-	public static final String VERSION = "2.6.0";
+	public static final String VERSION = "2.6.1";
 	/**
 	 * The version of Minecraft this was built for.
 	 */
-	public static final String MC_VERSION = "1.9";
+	public static final String MC_VERSION = "1.9.4";
 }
