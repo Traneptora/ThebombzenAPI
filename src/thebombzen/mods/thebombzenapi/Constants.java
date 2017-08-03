41e1b0836b72d0a7e8d91979f2110da090cd7e9e
Leo Izen <leo.izen@gmail.com>
2017-08-03 18:49:29 -0400

    Update hardcoded versions in Constants

--- src/thebombzen/mods/thebombzenapi/Constants.java
+++ src/thebombzen/mods/thebombzenapi/Constants.java
@@ -9,9 +9,9 @@ public interface Constants {
 	/**
 	 * The current version of ThebombzenAPI.
 	 */
-	public static final String VERSION = "2.6.1";
+	public static final String VERSION = "2.8.1";
 	/**
 	 * The version of Minecraft this was built for.
 	 */
-	public static final String MC_VERSION = "1.9.4";
+	public static final String MC_VERSION = "1.11.2";
 }
