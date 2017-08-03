9e893715ade3d30a8c3469f12014beed97b1f807
Leo Izen <leo.izen@gmail.com>
2017-08-03 19:00:14 -0400

    Add 1.11.2 to accepted versions

--- src/thebombzen/mods/thebombzenapi/ThebombzenAPI.java
+++ src/thebombzen/mods/thebombzenapi/ThebombzenAPI.java
@@ -50,7 +50,7 @@ import net.minecraftforge.fml.relauncher.SideOnly;
  * 
  * @author thebombzen
  */
-@Mod(modid = "thebombzenapi", name = "ThebombzenAPI", version = Constants.VERSION, guiFactory = "thebombzen.mods.thebombzenapi.client.ConfigGuiFactory", acceptedMinecraftVersions = "[1.9.4, 1.11]")
+@Mod(modid = "thebombzenapi", name = "ThebombzenAPI", version = Constants.VERSION, guiFactory = "thebombzen.mods.thebombzenapi.client.ConfigGuiFactory", acceptedMinecraftVersions = "[1.9.4, 1.11.2]")
 public class ThebombzenAPI extends ThebombzenAPIBaseMod {
 
 	/**
